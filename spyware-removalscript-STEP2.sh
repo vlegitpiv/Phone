26da93adf615fbec436b36b5fba6891fca863f67
subiet <42613406+vlegitpiv@users.noreply.github.com>
2024-01-31 21:29:24 +0100

    Create spyware-removalscript-STEP2.sh

---
+++ spyware-removalscript-STEP2.sh
@@ -0,0 +1,109 @@
+./adb shell pm uninstall -k --user 0 com.miui.cloudbackup
+./adb shell pm uninstall -k --user 0 com.miui.face
+./adb shell pm uninstall -k --user 0 com.mediatek.voiceunlock
+./adb shell pm uninstall -k --user 0 com.xiaomi.micloud.sdk
+./adb shell pm uninstall -k --user 0 com.mediatek.voicecommand
+./adb shell pm uninstall -k --user 0 com.miui.cloudservice
+./adb shell pm uninstall -k --user 0 com.ironsource.appcloud.oobe.hutchison
+./adb shell pm uninstall -k --user 0 com.miui.micloudsync
+./adb shell pm uninstall -k --user 0 com.miui.face.overlay.miui
+./adb shell pm uninstall -k --user 0 com.android.hotwordenrollment.xgoogle
+./adb shell pm uninstall -k --user 0 com.xiaomi.mipicks
+./adb shell pm uninstall -k --user 0 com.android.hotwordenrollment.okgoogle
+./adb shell pm uninstall -k --user 0 com.google.android.overlay.healthconnect
+./adb shell pm uninstall -k --user 0 com.goodix.gftest
+./adb shell pm uninstall -k --user 0 com.google.android.overlay.modules.documentsui
+./adb shell pm uninstall -k --user 0 com.mi.globalminusscreen
+./adb shell pm uninstall -k --user 0 com.xiaomi.xmsf
+./adb shell pm uninstall -k --user 0 com.milink.service
+./adb shell pm uninstall -k --user 0 com.google.android.gms.location.history
+./adb shell pm uninstall -k --user 0 com.miui.backup
+./adb shell pm uninstall -k --user 0 com.miui.analytics
+./adb shell pm uninstall -k --user 0 com.android.wallpaperbackup
+./adb shell pm uninstall -k --user 0 com.android.calllogbackup
+./adb shell pm uninstall -k --user 0 com.google.android.as
+./adb shell pm uninstall -k --user 0 com.google.android.as.oss
+./adb shell pm uninstall -k --user 0 com.miui.mishare.connectivity
+./adb shell pm uninstall -k --user 0 com.aura.oobe.deutsche
+./adb shell pm uninstall -k --user 0 com.android.sharedstoragebackup
+./adb shell pm uninstall -k --user 0 com.miui.bugreport
+./adb shell pm uninstall -k --user 0 com.miui.extraphoto
+./adb shell pm uninstall -k --user 0 org.mipay.android.manager
+./adb shell pm uninstall -k --user 0 com.google.android.apps.healthdata
+./adb shell pm uninstall -k --user 0 com.google.android.feedback
+./adb shell pm uninstall -k --user 0 com.google.android.ext.shared
+./adb shell pm uninstall -k --user 0 com.miui.yellowpage
+./adb shell pm uninstall -k --user 0 com.xiaomi.xmsfkeeper
+./adb shell pm uninstall -k --user 0 com.xiaomi.account
+./adb shell pm uninstall -k --user 0 com.miui.screenshot
+./adb shell pm uninstall -k --user 0 com.android.backupconfirm
+./adb shell pm uninstall -k --user 0 com.miui.cit
+./adb shell pm uninstall -k --user 0 cn.wps.xiaomi.abroad.lite
+./adb shell pm uninstall -k --user 0 com.miui.audiomonitor
+./adb shell pm uninstall -k --user 0 com.google.android.documentsui
+./adb shell pm uninstall -k --user 0 com.xiaomi.payment
+./adb shell pm uninstall -k --user 0 com.google.mainline.adservices
+./adb shell pm uninstall -k --user 0 com.google.mainline.telemetry
+./adb shell pm uninstall -k --user 0 com.google.android.gms.supervision
+./adb shell pm uninstall -k --user 0 com.miui.qr
+./adb shell pm uninstall -k --user 0 com.bsp.catchlog
+./adb shell pm uninstall -k --user 0 com.google.android.onetimeinitializer
+./adb shell pm uninstall -k --user 0 com.miui.audioeffect
+./adb shell pm uninstall -k --user 0 com.tencent.soter.soterserver
+./adb shell pm uninstall -k --user 0 com.fingerprints.sensortesttool
+./adb shell pm uninstall -k --user 0 com.miui.android.fashiongallery
+./adb shell pm uninstall -k --user 0 com.xiaomi.discover
+./adb shell pm uninstall -k --user 0 org.ifaa.aidl.manager
+./adb shell pm uninstall -k --user 0 com.miui.phrase
+./adb shell pm uninstall -k --user 0 com.xiaomi.joyose
+./adb shell pm uninstall -k --user 0 com.miui.msa.global
+./adb shell pm uninstall -k --user 0 com.google.android.marvin.talkback
+./adb shell pm uninstall -k --user 0 com.google.android.printservice.recommendation
+./adb shell pm uninstall -k --user 0 com.miui.daemon
+./adb shell pm uninstall -k --user 0 com.android.managedprovisioning
+./adb shell pm uninstall -k --user 0 com.android.ons
+./adb shell pm uninstall -k --user 0 com.android.providers.userdictionary
+./adb shell pm uninstall -k --user 0 com.fido.asm
+./adb shell pm uninstall -k --user 0 com.google.ambient.streaming
+./adb shell pm uninstall -k --user 0 com.xiaomi.mtb
+./adb shell pm uninstall -k --user 0 com.android.providers.partnerbookmarks
+./adb shell pm uninstall -k --user 0 com.android.wallpaper.livepicker
+./adb shell pm uninstall -k --user 0 com.android.printspooler
+./adb shell pm uninstall -k --user 0 com.google.android.partnersetup
+./adb shell pm uninstall -k --user 0 com.android.mms
+./adb shell pm uninstall -k --user 0 com.android.imsserviceentitlement
+./adb shell pm uninstall -k --user 0 com.android.managedprovisioning.overlay
+./adb shell pm uninstall -k --user 0 com.android.remoteprovisioner
+./adb shell pm uninstall -k --user 0 com.android.uwb.resources
+./adb shell pm uninstall -k --user 0 com.google.android.adservices.api
+./adb shell pm uninstall -k --user 0 com.google.android.ondevicepersonalization.services
+./adb shell pm uninstall -k --user 0 com.google.android.overlay.gmsconfig.asi
+./adb shell pm uninstall -k --user 0 com.google.android.overlay.gmsconfig.personalsafety
+./adb shell pm uninstall -k --user 0 com.google.android.overlay.gmsconfig.searchselector
+./adb shell pm uninstall -k --user 0 com.google.android.overlay.modules.captiveportallogin.forframework
+./adb shell pm uninstall -k --user 0 com.mediatek.mdmconfig
+./adb shell pm uninstall -k --user 0 com.mediatek.mdmlsample
+./adb shell pm uninstall -k --user 0 com.trustonic.teeservice
+#
+########################################################################################
+##### below commands might cause errors, but the java errors can be safely ignored #####
+########################################################################################
+./adb shell pm uninstall -k --user 0 com.miui.phone.carriers.overlay.vodafone
+./adb shell pm uninstall -k --user 0 com.aura.oobe.vodafone
+./adb shell pm uninstall -k --user 0 de.telekom.tsc
+./adb shell pm disable-user --user 0 com.altice.android.myapps
+./adb shell pm disable-user --user 0 com.orange.aura.oobe
+./adb shell pm disable-user --user 0 com.dti.telefonica
+./adb shell pm disable-user --user 0 com.sfr.android.sfrjeux
+./adb shell pm disable-user --user 0 com.orange.update
+./adb shell pm disable-user --user 0 com.ironsource.appcloud.oobe.hutchison
+./adb shell pm disable-user --user 0 com.dti.bouyguestelecom
+./adb shell pm disable-user --user 0 com.google.android.gms.supervision
+#
+###############################################################################################
+##### Before you can uninstall below package, you first need to replace your current      #####
+##### soft keyboard app in android. First install an alternative keyboard of your choice, #####
+##### and next go to <Input Method> and <Keyboard> and then activate the new keyboard.    #####
+##### Only run below line when you have installed and activated an alternative keyboard:  #####
+###############################################################################################
+#./adb shell pm uninstall -k --user 0 com.google.android.inputmethod.latin
