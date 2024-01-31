1d39f4cae720e229556b6fb9f3382f28f56d0366
subiet <42613406+vlegitpiv@users.noreply.github.com>
2024-01-31 21:25:53 +0100

    Create bloatware-removalscript-STEP1.sh

---
+++ bloatware-removalscript-STEP1.sh
@@ -0,0 +1,83 @@
+./adb shell pm uninstall -k --user 0 com.alibaba.aliexpresshd
+./adb shell pm uninstall -k --user 0 com.amazon.appmanager
+./adb shell pm uninstall -k --user 0 com.amazon.mShop.android.shopping
+./adb shell pm uninstall -k --user 0 com.google.android.apps.googleassistant
+./adb shell pm uninstall -k --user 0 com.audible.application
+./adb shell pm uninstall -k --user 0 com.microsoft.bing
+./adb shell pm uninstall -k --user 0 com.booking
+./adb shell pm uninstall -k --user 0 com.miui.calculator
+./adb shell pm uninstall -k --user 0 com.android.providers.calendar
+./adb shell pm uninstall -k --user 0 com.google.android.calendar
+./adb shell pm uninstall -k --user 0 com.xiaomi.cameratools
+./adb shell pm uninstall -k --user 0 com.android.camera
+./adb shell pm uninstall -k --user 0 com.android.cameraextensions
+./adb shell pm uninstall -k --user 0 com.android.chrome
+./adb shell pm uninstall -k --user 0 com.google.android.apps.chromecast.app
+./adb shell pm uninstall -k --user 0 com.android.deskclock
+./adb shell pm uninstall -k --user 0 com.miui.compass
+./adb shell pm uninstall -k --user 0 com.google.android.contacts
+./adb shell pm uninstall -k --user 0 com.android.providers.downloads
+./adb shell pm uninstall -k --user 0 com.android.providers.downloads.ui
+./adb shell pm uninstall -k --user 0 com.google.android.apps.docs
+./adb shell pm uninstall -k --user 0 com.facebook.services
+./adb shell pm uninstall -k --user 0 com.facebook.appmanager
+./adb shell pm uninstall -k --user 0 com.facebook.katana
+./adb shell pm uninstall -k --user 0 com.facebook.system
+./adb shell pm uninstall -k --user 0 com.mi.android.globalFileexplorer
+./adb shell pm uninstall -k --user 0 com.miui.mediaeditor
+./adb shell pm uninstall -k --user 0 com.miui.gallery
+./adb shell pm uninstall -k --user 0 com.xiaomi.glgm
+./adb shell pm uninstall -k --user 0 com.reachjunction.card.game.solitaire
+./adb shell pm uninstall -k --user 0 com.google.android.gm
+./adb shell pm uninstall -k --user 0 com.google.android.apps.subscriptions.red
+./adb shell pm uninstall -k --user 0 com.google.android.videos
+./adb shell pm uninstall -k --user 0 com.instagram.android
+./adb shell pm uninstall -k --user 0 com.linkedin.android
+./adb shell pm uninstall -k --user 0 com.google.android.apps.maps
+./adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+./adb shell pm uninstall -k --user 0 com.mi.globalbrowser
+./adb shell pm uninstall -k --user 0 com.opera.browser
+./adb shell pm uninstall -k --user 0 com.mi.global.shop
+./adb shell pm uninstall -k --user 0 com.miui.videoplayer
+./adb shell pm uninstall -k --user 0 com.miui.player
+./adb shell pm uninstall -k --user 0 com.netflix.mediaclient
+./adb shell pm uninstall -k --user 0 com.google.android.apps.magazines
+./adb shell pm uninstall -k --user 0 com.miui.notes
+./adb shell pm uninstall -k --user 0 com.opera.preinstall
+./adb shell pm uninstall -k --user 0 com.opera.browser
+./adb shell pm uninstall -k --user 0 com.google.android.apps.photos
+./adb shell pm uninstall -k --user 0 com.android.vending
+./adb shell pm uninstall -k --user 0 com.google.android.apps.podcasts
+./adb shell pm uninstall -k --user 0 com.qeeq.android
+./adb shell pm uninstall -k --user 0 com.android.soundrecorder
+./adb shell pm uninstall -k --user 0 com.miui.screenrecorder
+./adb shell pm uninstall -k --user 0 com.mediatek.callrecorder
+./adb shell pm uninstall -k --user 0 com.google.android.apps.safetyhub
+./adb shell pm uninstall -k --user 0 com.xiaomi.scanner
+./adb shell pm uninstall -k --user 0 com.miui.miservice
+./adb shell pm uninstall -k --user 0 com.xiaomi.midrop
+./adb shell pm uninstall -k --user 0 com.snapchat.android
+./adb shell pm uninstall -k --user 0 com.spotify.music
+./adb shell pm uninstall -k --user 0 com.zhiliaoapp.musically
+./adb shell pm uninstall -k --user 0 com.miui.weather2
+./adb shell pm uninstall -k --user 0 cn.wps.moffice_eng
+./adb shell pm uninstall -k --user 0 com.mi.global.bbs
+./adb shell pm uninstall -k --user 0 com.google.android.youtube
+./adb shell pm uninstall -k --user 0 com.google.android.apps.youtube.music
+./adb shell pm uninstall -k --user 0 com.google.android.apps.turbo
+./adb shell pm uninstall -k --user 0 com.google.android.apps.wellbeing
+./adb shell pm uninstall -k --user 0 com.google.android.apps.setupwizard.searchselector
+./adb shell pm uninstall -k --user 0 com.google.android.tts
+./adb shell pm uninstall -k --user 0 com.google.android.projection.gearhead
+./adb shell pm uninstall -k --user 0 com.google.android.setupwizard
+./adb shell pm uninstall -k --user 0 com.google.android.partnersetup
+./adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
+./adb shell pm disable-user --user 0 com.miui.calculator
+./adb shell pm disable-user --user 0 com.miui.compass   
+./adb shell pm disable-user --user 0 com.miui.notes  
+./adb shell pm disable-user --user 0 com.android.soundrecorder
+./adb shell pm disable-user --user 0 com.miui.screenrecorder  
+./adb shell pm disable-user --user 0 com.mediatek.callrecorder
+./adb shell pm disable-user --user 0 com.xiaomi.scanner       
+./adb shell pm disable-user --user 0 com.miui.weather2 
+./adb shell pm disable-user --user 0 com.xiaomi.midrop
